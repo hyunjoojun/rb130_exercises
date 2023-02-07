b3cb47c796f95f5c72b5f5763622e1446adb7ad9
Hyunjoo Jun <hjj102088@gmail.com>
2023-02-07 16:34:35 -0500

    Divisors

---
+++ easy1/divisors.rb
@@ -0,0 +1,12 @@
+def divisors(number)
+  1.upto(number).select do |num|
+    (number % num).zero?
+  end
+end
+
+p divisors(1) == [1]
+p divisors(7) == [1, 7]
+p divisors(12) == [1, 2, 3, 4, 6, 12]
+p divisors(98) == [1, 2, 7, 14, 49, 98]
+p divisors(99400891) == [1, 9967, 9973, 99400891] # may take a minute
+p divisors(999962000357)
