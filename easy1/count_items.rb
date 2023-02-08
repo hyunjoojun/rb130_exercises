362184c296a860ec734856af465043165ba064a3
Hyunjoo Jun <hjj102088@gmail.com>
2023-02-08 15:25:04 -0500

    Count

---
+++ easy1/count_items.rb
@@ -0,0 +1,14 @@
+def count(arr)
+  true_count = 0
+  0.upto(arr.size - 1) do |index|
+    true_count += 1 if yield(arr[index])
+  end
+  true_count
+end
+
+p count([1,2,3,4,5]) { |value| value.odd? } == 3
+p count([1,2,3,4,5]) { |value| value % 3 == 1 } == 2
+p count([1,2,3,4,5]) { |value| true } == 5
+p count([1,2,3,4,5]) { |value| false } == 0
+p count([]) { |value| value.even? } == 0
+p count(%w(Four score and seven)) { |value| value.size == 5 } == 2
