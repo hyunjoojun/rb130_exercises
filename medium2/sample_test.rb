a42de72482aee0cdcbfabc546ed2f21c4550d5ec
Hyunjoo Jun <hjj102088@gmail.com>
2023-02-28 13:30:50 -0500

    Review

--- medium2/sample_test.rb
+++ medium2/sample_test.rb
@@ -7,7 +7,8 @@ require_relative 'sample'
 class TextTest < Minitest::Test
   def setup
     @file = File.open('sample_text.txt', 'r')
-    @text = Text.new(@file.read)
+    @string = @file.read
+    @text = Text.new(@string)
   end
 
   def test_swap
@@ -15,12 +16,13 @@ class TextTest < Minitest::Test
     assert_equal(0, swapped_text.count('a'))
   end
 
+  def test_word_count
+    expected_count = @string.split.count
+    assert_equal(expected_count, @text.word_count)
+  end
+
   def teardown
     @file.close
     puts "File has been closed: #{@file.closed?}"
-  end
-
-  def test_word_count
-    assert_equal(72, @text.word_count)
   end
 end
