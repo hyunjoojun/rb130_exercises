c3dd98af0599f05c4a6437dc58fa0187a0a1b35d
Hyunjoo Jun <hjj102088@gmail.com>
2023-03-01 11:33:05 -0500

    Review

--- easy1/true_for_one.rb
+++ easy1/true_for_one.rb
@@ -6,6 +6,15 @@ def one?(collection)
   true_count == 1
 end
 
+# def one?(arr, &block)
+#   true_count = 0
+#   arr.each do |ele|
+#     true_count += 1 if block.call(ele)
+#     return false if true_count > 1
+#   end
+#   true_count == 1
+# end
+
 p one?([1, 3, 5, 6]) { |value| value.even? }    # -> true
 p one?([1, 3, 5, 7]) { |value| value.odd? }     # -> false
 p one?([2, 4, 6, 8]) { |value| value.even? }    # -> false
