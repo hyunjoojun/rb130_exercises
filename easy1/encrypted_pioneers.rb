f327890ff377d1968bcea6eb5aa36aee2ed19b07
Hyunjoo Jun <hjj102088@gmail.com>
2023-03-01 11:08:58 -0500

    Review

--- easy1/encrypted_pioneers.rb
+++ easy1/encrypted_pioneers.rb
@@ -22,16 +22,36 @@ ENCRYPTED_PIONEERS = [
   'Tregehqr Oynapu'
 ].freeze
 
-def rot13(text)
-  text.chars.map do |char|
-    if INPUT_CHARS.include?(char)
-      OUTPUT_CHARS[INPUT_CHARS.index(char)]
-    else
-      char
-    end
+# def rot13(text)
+#   text.chars.map do |char|
+#     if INPUT_CHARS.include?(char)
+#       OUTPUT_CHARS[INPUT_CHARS.index(char)]
+#     else
+#       char
+#     end
+#   end.join
+# end
+
+# ENCRYPTED_PIONEERS.each do |encrypted_name|
+#   puts rot13(encrypted_name)
+# end
+
+def rot13(string)
+  string.chars.map do |char|
+    decipher_char(char)
   end.join
 end
 
-ENCRYPTED_PIONEERS.each do |encrypted_name|
-  puts rot13(encrypted_name)
+def decipher_char(char)
+  if ('a'..'m').include?(char.downcase)
+    (char.ord + 13).chr
+  elsif ('n'..'z').include?(char.downcase)
+    (char.ord - 13).chr
+  else
+    char
+  end
+end
+
+ENCRYPTED_PIONEERS.each do |name|
+  puts rot13(name)
 end
