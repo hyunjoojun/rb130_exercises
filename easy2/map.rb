548fb473c37c0a3200cfc21fc6098421b640217f
Hyunjoo Jun <hjj102088@gmail.com>
2023-03-01 12:26:05 -0500

    Review

--- easy2/map.rb
+++ easy2/map.rb
@@ -4,9 +4,21 @@ def map(arr)
   result
 end
 
-p map([1, 3, 6]) { |value| value**2 } == [1, 9, 36]
-p map([]) { |value| true } == []
-p map(['a', 'b', 'c', 'd']) { |value| false } == [false, false, false, false]
-p map(['a', 'b', 'c', 'd']) { |value| value.upcase } == ['A', 'B', 'C', 'D']
-p map([1, 3, 4]) { |value| (1..value).to_a } == [[1], [1, 2, 3], [1, 2, 3, 4]]
-p map({ a: 1, b: 2 }) { |_, value| value += 1 }
+def map2(arr)
+  arr.each_with_object([]) do |ele, result|
+    result << yield(ele)
+  end
+end
+
+def map3(arr, &block)
+  arr.each_with_object([]) do |ele, result|
+    result << block.call(ele)
+  end
+end
+
+p map3([1, 3, 6]) { |value| value**2 } == [1, 9, 36]
+p map3([]) { |value| true } == []
+p map3(['a', 'b', 'c', 'd']) { |value| false } == [false, false, false, false]
+p map3(['a', 'b', 'c', 'd']) { |value| value.upcase } == ['A', 'B', 'C', 'D']
+p map3([1, 3, 4]) { |value| (1..value).to_a } == [[1], [1, 2, 3], [1, 2, 3, 4]]
+p map3({ a: 1, b: 2 }) { |_, value| value += 1 }
