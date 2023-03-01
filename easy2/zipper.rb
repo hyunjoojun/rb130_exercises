93f8380e8a558cea8085a31bd4088d926c2779d6
Hyunjoo Jun <hjj102088@gmail.com>
2023-03-01 12:19:38 -0500

    Review

--- easy2/zipper.rb
+++ easy2/zipper.rb
@@ -6,4 +6,12 @@ def zip(arr1, arr2)
   result
 end
 
+def zip2(arr1, arr2)
+  result = []
+  arr1.each_with_index do |ele, idx|
+    result << [ele, arr2[idx]]
+  end
+  result
+end
+
 p zip([1, 2, 3], [4, 5, 6]) == [[1, 4], [2, 5], [3, 6]]
